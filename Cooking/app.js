382e8ce7d1c5249cd72f87c4e5f7bfe4106418d7
Ayush Rameja <Ayushrwork@gmail.com>
2020-09-27 17:25:43 +0530

    major changes in the main page and added the cooking section

---
+++ Cooking/app.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector(".fas");
+const navLInks = document.querySelector(".nav-links");
+const links = document.querySelectorAll("#link-hover");
+
+
+burger.addEventListener("click", () => {
+    navLInks.classList.toggle("active");
+
+    if (navLInks.classList.contains("active")) {
+        burger.classList.replace("fa-hamburger", "fa-drumstick-bite");
+    } else {
+        burger.classList.replace("fa-drumstick-bite", "fa-hamburger");
+    }
+})
+
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        link.innerHTML = "I want to try";
+    })
+})
+
+
+// when we click on the navlink it will show the main page, the menu will not stay on the 
+
+function showPage() {
+    navLInks.classList.remove("active");
+}
--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+
+burger.addEventListener("click", () => {
+    navLinks.classList.toggle('nav-active');
+
+    if (navLinks.classList.contains("nav-active")) {
+        burger.classList.add('toogle');
+    } else {
+        burger.classList.remove('toogle');
+    }
+});
+
+function showPage() {
     const navLinks = document.querySelector('.nav-links');
-
-    burger.addEventListener("click", () => {
-        navLinks.classList.toggle('nav-active')
-        // burgur animation
-        burger.classList.toggle('toogle');
-    });
-
+    navLinks.classList.remove("nav-active");
 }
-navSlide();
